2d20167e1a43c42c1173cf8fb1d6d88c5d5c0400
vitormoscoso <vitormoscoso@gmail.com>
2023-12-12 20:33:17 -0300

    ♻️ refactoring card

--- src/components/CardComponent/index.tsx
+++ src/components/CardComponent/index.tsx
@@ -1,38 +1,40 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-} from "@mui/material";
-export default function CardComponent({ data }: any) {
-  console.log("card: ", data);
+import { Button, Divider, Paper, Typography } from "@mui/material";
 
+export default function CardComponent({ data }: any) {
   return (
-    <Card
-      variant="outlined"
+    <Paper
+      elevation={3}
       sx={{
-        width: "100%",
-        height: "100%",
+        margin: 2,
+        padding: 2,
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
-        alignItems: "center",
+        width: "20%",
       }}
     >
-      <CardContent>
-        <Typography
-          variant="h5"
-          component="div"
-          sx={{ display: "flex", justifyContent: "center" }}
-        >
-          {data?.value}
-        </Typography>
-        <Typography variant="body2">{data?.timestamp}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Histórico</Button>
-      </CardActions>
-    </Card>
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        Última medição
+      </Typography>
+      <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}
+      >
+        {data?.value}
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        sx={{ marginTop: 1, display: "flex", justifyContent: "center" }}
+      >
+        {data?.timestamp}
+      </Typography>
+      <Button sx={{ marginTop: 1 }}>Histórico</Button>
+    </Paper>
   );
 }
