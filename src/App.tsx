97cd848a63d70c3496b1c4eda4ca3c98667bbb1b
vitormoscoso <vitormoscoso@gmail.com>
2023-12-12 19:59:52 -0300

    integrating router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
