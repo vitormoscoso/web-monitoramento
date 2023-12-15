4a5506bc581f7f7359d90102797f5c4e10da1f28
vitormoscoso <vitormoscoso@gmail.com>
2023-12-14 22:03:09 -0300

    Remove base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-monitoramento/',
   server: {
     host: "0.0.0.0", // Isso faz o servidor aceitar conexões de qualquer IP
   },
