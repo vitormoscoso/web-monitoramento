ba04195931530ddad4575d36c5282e331c638a1e
Vitor Moscoso <vitormoscoso@gmail.com>
2023-12-17 12:09:24 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/web-monitoramento/",
   server: {
     host: "0.0.0.0", // Isso faz o servidor aceitar conexões de qualquer IP
   },
